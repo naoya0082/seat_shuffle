cf89c4307d5f4ac520669ab927b74db67ccfaf61
naoya0082 <6ds3f7@gmail.com>
2018-11-27 10:50:33 +0900

    大平さんへの質問 Tableごとのランダム表示はできたが、他のテーブルで名前が重複してしまう
    Table1~3で名前が重複しないためのヒントが欲しい！

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
+import random
+
+
 def main():
     f = open("members.txt", mode="r")
     text = f.read()
-    print(text)
 
     f.close()
+
+    # テキストを改行で区切ってリスト化
+    lines = text.split("\n")
+    print(lines)
+
+    print("=================================================================================================")
+
+    # 改行(\n)で区切る(split) <- OK
+
+    # random.sample(x, k) <- 重複無しのランダム
+    print(f"Table1: {random.sample(lines, 6)}")
+    print(f"Table2: {random.sample(lines, 5)}")
+    print(f"Table3: {random.sample(lines, 4)}")
+
+    # print(f"Table2: {random.choice(lines)}, {random.choice(lines)}, {random.choice(lines)}")
+    # print(f"Table3: {random.choice(lines)}, {random.choice(lines)}, {random.choice(lines)}")
+
+    print("=================================================================================================")
 
 
 if __name__ == "__main__":
